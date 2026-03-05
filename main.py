e53a670f03329624ee170dab3eafd7c61fc4e478
1ampupa <craftpupathai@gmail.com>
2026-03-05 20:20:38 +0700

    Add quiz generator for all operator

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from module.quiz import Quiz
 from module.operator import Operator
 
-print(Quiz(1, 5, Operator.ADDITION))
+Quiz.debug_mode = True
+
+quiz1 = Quiz.generate(Operator.ADDITION)
+print(quiz1)
+quiz2 = Quiz.generate(Operator.SUBTRACTION)
+print(quiz2)
+quiz3 = Quiz.generate(Operator.MULTIPLICATION)
+print(quiz3)
+quiz4 = Quiz.generate(Operator.DIVISION)
+print(quiz4)
