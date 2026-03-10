a21a2a8fb1331a42d7a3e05611bd7dc9a2858af9
1ampupa <craftpupathai@gmail.com>
2026-03-11 01:50:24 +0700

    Add Streak system and telemetry reports after each session

--- module/session.py
+++ module/session.py
@@ -1,3 +1,4 @@
+import time
 from module.operator import Operator
 
 class Session:
@@ -9,12 +10,14 @@ class Session:
 
         # Telemetry
         self.question_answered = 0
+        self.points = 0
         self.correct_answer = 0
         self.incorrect_answer = 0
         self.correct_streak = 0
         self.max_correct_streak = 0
-        self.accuracy_percentage = 100
-        self.time_per_question = 0
+        self.accuracy_percentage = 0
+        self.time_elasped = 0
+        self.average_time_per_question = 0
 
         Session.session_id_counter += 1
 
@@ -22,6 +25,7 @@ class Session:
         if self.active_user is not None: # Preventing overlapping users in the same session
             print(f"This session is occupied by {self.active_user.name}")
             return
+        
         if user.current_session is not None:    
             print(f"This user is already in the other session ({user.current_session.id}).")
             return
@@ -31,6 +35,15 @@ class Session:
 
     def disconnect_user(self):
         if self.active_user is not None:
+            # Calculate Accuracy and
+            if self.question_answered != 0:
+                self.accuracy_percentage = round((self.correct_answer / self.question_answered) * 100)
+                self.average_time_per_question = self.time_elasped / self.question_answered
+            else:
+                self.accuracy_percentage = 0
+                self.average_time_per_question = 0  
+            # Creating telemetry summary report
+            self.summarise_telemetry()
             print(f"Disconnecting {self.active_user.name} from session {self.id}.")
             self.active_user.current_session = None
         self.active_user = None
@@ -48,15 +61,25 @@ class Session:
         print("Session is ready!\nAnswer the questions correctly.\n")
 
         while True:
+            # Generate Quiz
             quiz = Quiz.generate(self.active_user, Operator.MULTIPLICATION)
             print(quiz)
 
+            # Start Quiz Timer
+            quiz_start_time = time.perf_counter()
+
+            # Receive User Answer
             answer = input("Answer for world peace: ")
+
+            # Game Quit Handler (temporary)
             if answer.strip().lower() == 'q':
                 self.disconnect_user()
                 print("You quit the game. Thanks for saving world peace.")
                 break
             
+            quiz_time_taken = time.perf_counter() - quiz_start_time
+
+            # Check User Answer
             try:
                 result = UserAnswer(self.active_user, quiz, int(answer))
             except ValueError:
@@ -65,4 +88,50 @@ class Session:
             result.update_difficulty()
 
             print(f"{result}! The answer is {result.quiz.answer}.\n")
+
+            self.update_telemetry(result.is_correct, quiz_time_taken)
+
+    def update_telemetry(self, is_correct: bool, time_elapsed: float) -> None:
+        # Update Statstic
+        self.question_answered += 1
+        self.time_elasped += time_elapsed
+
+        if is_correct:
+            self.points += 1 # Base point for correct answer
+            self.correct_answer += 1
+            self.correct_streak += 1
+
+            if self.correct_streak % 3 == 0:
+                # Annouce streak every 3 correct answers
+                print(f"Correct {self.correct_streak} times in a row!")
+
+            if self.correct_streak >= 3:
+                # Bonus +1 Point when streak is 3+
+                self.points += 1
+
+            if self.correct_streak > self.max_correct_streak: # Record max streak
+                self.max_correct_streak = self.correct_streak
+        else:
+            self.points -= 1 # Base point for incorrect answer
+            self.incorrect_answer += 1
+
+            if self.correct_streak >= 3:
+                # Annouce end of streak if user has 3+ streak
+                print("Streak ended!")
+
+            self.correct_streak = 0
     
+    def summarise_telemetry(self) -> None:
+        if self.active_user is not None:
+            print(
+                f"{"="*50}\n"
+                f"Session Summary Report for session {self.id}\n",
+                f"User: {self.active_user.name}\n{self.points} Points!\nTime elapsed: {self.time_elasped:.2f} seconds\n\n"
+                f"Question answered: {self.question_answered}\n",
+                f"Accuracy: {self.accuracy_percentage} % ({self.correct_answer}/{self.question_answered})\n",
+                f"Max streak: {self.max_correct_streak}\n",
+                f"Avg. Time/Question: {self.average_time_per_question:.2f} seconds\n",
+                f"{"="*50}"
+            )
+        else:
+            print("Failed to generate a session summary report without any active user.")
